27a9b0fa5f112a7781d63e5cc0587b786bdead2a
ThomasLuton <luton.thomas60@gmail.com>
2024-07-25 16:29:23 +0200

    start documentation and experiment mark down

--- PokeAppDocs/docusaurus.config.js
+++ PokeAppDocs/docusaurus.config.js
@@ -8,8 +8,8 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'PokeApp Documentation',
+  tagline: 'Technical documentation for PokeApp team',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
